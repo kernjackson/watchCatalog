c64cbdd83f02b10a38248fbedca1a9b0270d311a
Kern Jackson <kernjackson@gmail.com>
2025-06-09 18:08:22 -0700

    Initial Commit

---
+++ watchCatalog Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  watchCatalog Watch App
+//
+//  Created by Kern Jackson on 6/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ watchCatalog Watch App/watchCatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchCatalogApp.swift
+//  watchCatalog Watch App
+//
+//  Created by Kern Jackson on 6/9/25.
+//
+
+import SwiftUI
+
+@main
+struct watchCatalog_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
